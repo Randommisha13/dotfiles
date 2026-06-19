891c0e836960be55202dd11c382e1f278aed0ef4
Randommisha13 <52196819+Randommisha13@users.noreply.github.com>
2026-06-19 20:27:29 +0500

    Update .config/nvim/lua/configs/lspconfig.lua

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,14 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "pyright", "ltex-ls-plus", "pretty-php" }
+local servers = {
+    "cssls",
+    "html",
+    "ltex-ls-plus",
+    "pretty-php",
+    "pyright",
+    "nil",
+    "taplo",
+}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
