6c5ac3240d806a3b4ce9f5684e9fb4c76d128e16
Randommisha13 <52196819+Randommisha13@users.noreply.github.com>
2026-07-27 21:55:31 +0500

    Update .config/fastfetch/systemctl-unit-stats.py Add .config/user-tmpfiles.d/ssh-host-sockets.conf Add .config/user-tmpfiles.d/tmux-tmpdirs.conf

--- dot_config/fastfetch/executable_systemctl-unit-stats.py
+++ dot_config/fastfetch/executable_systemctl-unit-stats.py
@@ -1,34 +1,41 @@
 #!/usr/bin/env python
 import re
+import sys
+import curses
 import subprocess
 
 checked_fields = ["Units", "Jobs", "Failed"]
 
-systemctl_status = subprocess.check_output(['systemctl', 'status']).decode(encoding='utf-8')
+systemctl_out = subprocess.run(['systemctl', 'status'], capture_output=True)
 
-fields_and_values = {}
+if systemctl_out.returncode > 0:
+    print(f"systemctl exited with status {systemctl_out.returncode}", file=sys.stderr)
+    exit(systemctl_out.returncode)
+
+stdout = systemctl_out.stdout.decode(encoding="utf-8")
+
+parsed_output = {}
 for field in checked_fields:
-    match = re.search(rf"^\s*{field}:\s+(\d+).*$", systemctl_status, re.MULTILINE)
+    match = re.search(rf"^\s*{field}:\s+(\d+).*$", stdout, re.MULTILINE)
     if match:
-        fields_and_values[field] = match.group(1)
+        parsed_output[field] = match.group(1)
 
-if int(fields_and_values["Failed"]) != 0:
+
+output_strings = []
+for field, value in parsed_output.items():
+    output_strings.append(f"{value} {field.lower()}")
+
+curses.setupterm()
+reset_seq = curses.tparm(curses.tigetstr("sgr0")).decode(encoding="utf-8")
+bold_seq = curses.tparm(curses.tigetstr("bold")).decode(encoding="utf-8")
+white_fg_seq = curses.tparm(curses.tigetstr("setaf"), 7).decode(encoding="utf-8")
+red_bg_seq = curses.tparm(curses.tigetstr("setab"), 1).decode(encoding="utf-8")
+
+if int(parsed_output["Failed"]) > 0:
+    print(f"{bold_seq}{red_bg_seq}{white_fg_seq}{", ".join(output_strings)}{reset_seq}")
     exit_status = 1
 else:
+    print(f"{", ".join(output_strings)}")
     exit_status = 0
-
-output_string = ""
-is_first = True
-for field, value in fields_and_values.items():
-    if is_first:
-        output_string += f"{value} {field.lower()}"
-        is_first = False
-    else:
-        output_string += f", {value} {field.lower()}"
-if exit_status == 0:
-    print(output_string)
-else:
-    # White foreground, red background, bold
-    print(f"\033[37;41;1m{output_string}\033[0m")
 
 exit(exit_status) 
